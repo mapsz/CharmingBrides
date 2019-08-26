831106415c8e6306ccd17bdbd899c1950f178616
Jurij Gergelaba <jurij.gergelaba@gmail.com>
2019-08-26 05:59:30 +0300

    main page services fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password'))->nullable();
             $table->smallInteger('role')->default(0)->comment(
                 "-1:ban, 0:unconfirm, 1:girl, 2:man, 3:moder, 4:admin"
             );
