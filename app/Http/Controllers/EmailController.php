367f3c2a9708df21bad7f53714ed55d70438bd14
Jurij Gergelaba <jurij.gergelaba@gmail.com>
2019-11-30 12:42:58 +0200

    email fix

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -68,7 +68,7 @@ class EmailController extends Controller
       $d['content'] = $data['content'];
 
 
-      $this->dispatch((new EmailSendJob($d)->onQueue('low')));
+      $this->dispatch((new EmailSendJob($d))->onQueue('low'));
     }
 
     return response()->json(['error' => '0','data' => 123]);
